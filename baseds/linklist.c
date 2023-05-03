75745c303d842a9df9b70c4861b94a3689c57e8d
syz-lm <congshi.mua@outlook.com>
2023-05-03 09:27:06 +0800

    fix

--- baseds/linklist.c
+++ baseds/linklist.c
@@ -30,26 +30,67 @@ void insert(struct LinkList *L, struct Node *x) {
 		x -> prev = NULL;
 		x -> next = NULL;
 	} else {
-		x -> next = L -> head -> prev;
+		x -> next = L -> head;
 		L -> head -> prev = x;
 		x -> prev = NULL;
 	}
+	L -> size ++;
 }
 
 void delete(struct LinkList *L, struct Node *x) {
 	struct Node *head = L -> head;
 	struct Node *prev, *next;
-	while(head -> next != NULL) {
+	while(head != NULL) {
 		if (head == x) {
+			// 这里原指针没有回收，程序会自动回收吗？
 			prev = head -> prev;
 			next = head -> next;
 
 			prev -> next = next;
 			next -> prev = prev;
+			L -> size --;
 		}
 		head = head -> next;
 	}
 }
 
+void print(struct LinkList *L) {
+	struct Node *head = L -> head;
+	while (head != NULL) {
+		printf("%d\n", head -> key);
+		head = head -> next;		
+	}
+}
+
+/**
+ * 测试
+ */
 int main() {
+	struct LinkList L;
+
+	init(&L);
+
+	struct Node x;
+	x.prev = NULL;
+	x.next = NULL;
+	x.key = 1;
+	insert(&L, &x);
+
+	struct Node x1;
+	x1.prev = NULL;
+	x1.next = NULL;
+	x1.key = 2;
+	insert(&L, &x1);
+
+	struct Node x2;
+	x2.prev = NULL;
+	x2.next = NULL;
+	x2.key = 2;
+	insert(&L, &x2);
+
+	print(&L);
+
+	print("删除-----------------\n");
+	delete(x2);
+	print(&L);
 }
